56fb2164e739605e19daad07adfb577b6675f677
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-08-17 06:01:22 -0300

    Create index.tsx

---
+++ src/components/SideMenu/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+
+import * as S from './styles';
+
+/* criando uma declaração global */
+declare global {
+	interface Window {
+		toggleActiveMenu: (() => void) | undefined;
+	}
+}
+
+const scrollThreshold = 300;
+
+const SideMenu: React.FC = ({ children }) => {
+	const [isActive, setIsActive] = useState(false);
+	const [scrollY, setScrollY] = useState(0);
+
+	useEffect(() => {
+		function onScroll() {
+			setScrollY(window.scrollY);
+			setIsActive(false);
+		}
+
+		window.addEventListener('scroll', onScroll);
+
+		return () => window.removeEventListener('scroll', onScroll);
+	}, [scrollY]);
+
+	const classes = [
+		isActive ? 'open' : '',
+		scrollY <= scrollThreshold ? 'scrollOpen' : '',
+	];
+	const className = classes.join(' ').trim();
+
+	function toggleActiveMenu() {
+		setIsActive((prev) => !prev);
+	}
+
+	window.toggleActiveMenu = toggleActiveMenu;
+
+	return <S.Container className={className}>{children}</S.Container>;
+};
+
+export default SideMenu;
